c75578fd2284e0cfd751ec5a2a4c6f96999124db
Jeroen Mies <jeroen.mies@gmail.com>
2017-07-27 09:47:41 +0100

    Add files via upload

--- src/test_support/AppTestView.js
+++ src/test_support/AppTestView.js
@@ -10,9 +10,19 @@ export class AppTestView {
     return this.screen.find('[data-qa="counter-value"]');
   }
 
-  clickIncrementCounter() {
-    const button = this.screen.find('[data-qa="counter-value-inc"]');
-    click(button);
-  }
+    clickIncrementCounter() {
+        const button = this.screen.find('[data-qa="counter-value-inc"]');
+        click(button);
+    }
+
+    clickDecrementCounter() {
+        const button = this.screen.find('[data-qa="counter-value-dec"]');
+        click(button);
+    }
+
+    clickResetCounter() {
+        const button = this.screen.find('[data-qa="counter-value-reset"]');
+        click(button);
+    }
 
 }
